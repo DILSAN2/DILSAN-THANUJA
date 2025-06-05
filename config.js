fd0688e0a651b394974d191c414f000a72eff8af
DILSAN2 <dilsanthanuja2011@gmail.com>
2025-06-05 17:55:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'dilsanthanuja2011@gmail.com'
-global.github = 'https://github.com/DILSAN-THANUJAabot1/DILSAN-THANUJA'
+global.github =https://github.com/DILSAN2/DILSAN-THANUJA.git'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.sudo = process.env.SUDO || '94702509233'
 global.devs = '94702509233';
-global.website = 'https://github.com/DILSAN-THANUJAabot1/DILSAN-THANUJA' //wa.me/+25000000000000
+global.website = 'https://github.com/DILSAN2/DILSAN-THANUJA.git' //wa.me/+25000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/fb7993e24f7ecdaecbed5.jpg'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'DILSAN-THANUJA' : process.env.BOT_NAME,
